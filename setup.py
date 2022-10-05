428d022a68e1d62b91d4b87f2b8297a173442860
ricomnl <rmeinl97@gmail.com>
2022-10-05 11:15:32 -0700

    separate package for retro

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name="streamlit-plotly-events",
-    version="0.0.6",
+    name="streamlit-plotly-events-retro",
+    version="0.0.8",
     author="Ellie Jones",
     author_email="ellie@altaml.com",
     description="Plotly chart component for Streamlit that also allows for events to bubble back up to Streamlit.",
     long_description="Plotly chart component for Streamlit that also allows for events to bubble back up to Streamlit.",
     long_description_content_type="text/plain",
-    url="https://github.com/null-jones/streamlit-plotly-events",
+    url="https://github.com/ricomnl/streamlit-plotly-events",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     include_package_data=True,
